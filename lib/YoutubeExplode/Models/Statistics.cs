a0f0f20097a2b20b1955e12ad4797412d5ce7fec
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-10-29 16:54:37 +0700

    a

---
+++ lib/YoutubeExplode/Models/Statistics.cs
@@ -0,0 +1,54 @@
+﻿using ProtoBuf;
+using YoutubeExplode.Internal;
+
+namespace YoutubeExplode.Models
+{
+    /// <summary>
+    /// User activity statistics.
+    /// </summary>
+    [ProtoContract]
+    public class Statistics
+    {
+        /// <summary>
+        /// View count.
+        /// </summary>
+        [ProtoMember(1)]
+        public long ViewCount { get; set; }
+
+        /// <summary>
+        /// Like count.
+        /// </summary>
+        [ProtoMember(2)]
+        public long LikeCount { get; set; }
+
+        /// <summary>
+        /// Dislike count.
+        /// </summary>
+        [ProtoMember(3)]
+        public long DislikeCount { get; set; }
+
+        /// <summary>
+        /// Average user rating in stars (1 star to 5 stars).
+        /// </summary>
+        [ProtoMember(4)]
+        public double AverageRating
+        {
+            get
+            {
+                if (LikeCount + DislikeCount == 0) return 0;
+                return 1 + 4.0 * LikeCount / (LikeCount + DislikeCount);
+            }
+            set { }
+        }
+
+        public Statistics() { }
+
+        /// <summary />
+        public Statistics(long viewCount, long likeCount, long dislikeCount)
+        {
+            ViewCount = viewCount.GuardNotNegative(nameof(viewCount));
+            LikeCount = likeCount.GuardNotNegative(nameof(likeCount));
+            DislikeCount = dislikeCount.GuardNotNegative(nameof(dislikeCount));
+        }
+    }
+}
---
+++ lib/YoutubeExplode/Models/Video.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+//using ProtoBuf;
+using YoutubeExplode.Internal;
+
+namespace YoutubeExplode.Models
+{
+    /// <summary>
+    /// Information about a YouTube video.
+    /// </summary>
+    //[ProtoContract]
+    //[ProtoInclude(6, typeof(ThumbnailSet))]
+    //[ProtoInclude(9, typeof(Statistics))]
+    public class Video
+    {
+        /// <summary>
+        /// ID of this video.
+        /// </summary>
+        [NotNull]
+        //[ProtoMember(1)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Author of this video.
+        /// </summary>
+        [NotNull]
+        //[ProtoMember(2)]
+        public string Author { get; set; }
+
+        /// <summary>
+        /// Upload date of this video.
+        /// </summary>
+        //[ProtoMember(3)]
+        public DateTimeOffset UploadDate { get; set; }
+
+        /// <summary>
+        /// Title of this video.
+        /// </summary>
+        [NotNull]
+        //[ProtoMember(4)]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Description of this video.
+        /// </summary>
+        [NotNull]
+        //[ProtoMember(5)]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Thumbnails of this video.
+        /// </summary>
+        [NotNull]
+        //[ProtoMember(6)]
+        public ThumbnailSet Thumbnails { get; set; }
+
+        /// <summary>
+        /// Duration of this video.
+        /// </summary>
+        //[ProtoMember(7)]
+        public TimeSpan Duration { get; set; }
+
+        /// <summary>
+        /// Search keywords of this video.
+        /// </summary>
+        [NotNull, ItemNotNull]
+        //[ProtoMember(8)]
+        public List<string> Keywords { get; set; }
+
+        /// <summary>
+        /// Statistics of this video.
+        /// </summary>
+        [NotNull]
+        //[ProtoMember(9)]
+        public Statistics Statistics { get; set; }
+
+        public Video() { }
+
+        /// <summary />
+        public Video(string id, string author, DateTimeOffset uploadDate, string title, string description,
+            ThumbnailSet thumbnails, TimeSpan duration, List<string> keywords, Statistics statistics)
+        {
+            Id = id.GuardNotNull(nameof(id));
+            Author = author.GuardNotNull(nameof(author));
+            UploadDate = uploadDate;
+            Title = title.GuardNotNull(nameof(title));
+            Description = description.GuardNotNull(nameof(description));
+            Thumbnails = thumbnails.GuardNotNull(nameof(thumbnails));
+            Duration = duration.GuardNotNegative(nameof(duration));
+            Keywords = keywords.GuardNotNull(nameof(keywords));
+            Statistics = statistics.GuardNotNull(nameof(statistics));
+        }
+
+        /// <inheritdoc />
+        public override string ToString() => Title;
+    }
+}
