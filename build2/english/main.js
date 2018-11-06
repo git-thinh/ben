7ecd9c9f93b4a0c2a58b2c6d072aa1c0b823c4b3
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-11-06 17:16:01 +0700

    a

--- build2/english/main.js
+++ build2/english/main.js
@@ -1,6 +1,71 @@
 ﻿var f_log = 1 ? console.log.bind(console, 'UI: ') : function () { };
 function f_get(url) { var r = new XMLHttpRequest(); r.open('GET', url, false); r.send(null); if (r.status === 200) return r.responseText; return ''; }
 
+
+
+// #region VUE: BROADCAST MESSAGE ...
+
+var BROADCAST_COMPONENTS_VUE;
+function f_broadCastVue_init() {
+    var _divBroadCast = document.createElement('div');
+    _divBroadCast.id = 'broadcast-xxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+    document.body.appendChild(_divBroadCast);
+    BROADCAST_COMPONENTS_VUE = new Vue({
+        el: '#' + _divBroadCast.id,
+        methods: {
+            f_sendMessage_toHomeUI: function (data) {
+                this.$emit('BROADCAST_COMPONENTS_VUE_REG_HOMEUI', data);
+            },
+            f_sendMessage_toAll: function (data) {
+                this.$emit('BROADCAST_COMPONENTS_VUE', data);
+            },
+            f_sendMessage_toSender: function (channelSender, data) {
+                this.$emit(channelSender, data);
+            },
+        }
+    });
+}
+f_broadCastVue_init();
+
+function f_hui_broadcast_ComponentsVUE_Join_Event(f_callback) {
+    BROADCAST_COMPONENTS_VUE.$on('BROADCAST_COMPONENTS_VUE_REG_EVENT', f_callback);
+}
+
+function f_hui_broadcast_ComponentsVUE_Join_HomeUI(f_callback) {
+    BROADCAST_COMPONENTS_VUE.$on('BROADCAST_COMPONENTS_VUE_REG_HOMEUI', f_callback);
+}
+
+function f_hui_broadcast_ComponentsVUE_Join_All(f_callback) {
+    BROADCAST_COMPONENTS_VUE.$on('BROADCAST_COMPONENTS_VUE', f_callback);
+}
+
+// vueCom = {id, name}
+function f_hui_broadcast_ComponentsVUE_Join_Sender(f_callback, _self) {
+    var channelSender;
+    if (_self != null && _self._channelBroadcast != null)
+        channelSender = _self._channelBroadcast.getName();
+    if (channelSender != null)
+        BROADCAST_COMPONENTS_VUE.$on(channelSender, f_callback);
+    //console.log('channelSender = ', channelSender)
+}
+
+function f_vue_broadcast_message_Receiver(_self, msg, f_callback) {
+    //console.log('VUE_COMPONENTS.' + _self._channelBroadcast.getName() + ' = ', msg);
+    if (msg != null) {
+        if (msg.CALL != null) {
+            if (_self[msg.CALL] != null) _self[msg.CALL](msg);
+            return;
+        }
+    }
+
+    f_callback(msg);
+}
+
+// #endregion
+
 var _profile;
 var _app;
 var _page = '<button @click="f_destroy">Destroy MAIN</button> <br><button @click="f_com1">com1: load</button> | <button @click="f_com2">com2: load</button> | <button @click="f_com3">com3: dynamic</button> | <button @click="f_com4">com4: Profile</button>  <hr> '
@@ -11,6 +76,11 @@ var _mixin = {
         f_log('Class Base component created ... data = ', JSON.stringify(this.$data));
     },
     methods: {
+        f_broadCast_toVue: function (m) {
+            f_log('----> component send broadcast to vue: ', m);
+            //this.$dispatch('BROADCAST_TO_VUE', m);
+            _app.$emit('BROADCAST_TO_VUE', m);
+        },
         f_destroy: function () {
             this.$destroy();
         },
@@ -33,7 +103,7 @@ var _mixin = {
 
             // create reusable constructor
             var Profile = Vue.extend({
-                template: '<p>{{firstName}} {{lastName}} aka {{alias}}</p><button @click="f_destroy">Destroy Profile</button> <br>  ' + _page
+                template: '<p>{{firstName}} {{lastName}} aka {{alias}}</p><button @click="f_destroy">Destroy Profile</button> <button @click="f_broadCast_toVue({time: new Date().toString()})">Profile send f_broadCast_toVue </button><br>  ' + _page
             });
 
             // create an instance of Profile
@@ -54,7 +124,32 @@ var _mixin = {
                     f_destroy: function () {
                         f_log('destroy _profile ...');
                         _profile.$destroy();
-                    }
+                    },
+                    // #region [ SCREEN: BROADCAST ]
+
+                    f_broadcas_Register: function () {
+                        var _self = this;
+                        this._channelBroadcast = { id: this._uid, name: 'BROADCAST_COMPONENTS_VUE_REG_HOMEUI', getName: function () { return this.name; } };
+                        f_hui_broadcast_ComponentsVUE_Join_All(function (msg) { f_vue_broadcast_message_Receiver(_self, msg, _self.f_com_process_Message_Receiver); });
+                        f_hui_broadcast_ComponentsVUE_Join_Sender(function (msg) { f_vue_broadcast_message_Receiver(_self, msg, _self.f_com_process_Message_Receiver); }, _self);
+                        f_hui_broadcast_ComponentsVUE_Join_Event(function (msg) { f_vue_broadcast_message_Receiver(_self, msg, _self.f_com_even_Message_Receiver); }, _self);
+                    },
+
+                    f_com_process_Message_Receiver: function (msg) {
+                        var _self = this;
+                        //////f_log_hui('HOMEUI -> RECEIVER: ', msg);
+                        var key = msg.KEY, data = msg.DATA;
+                        switch (key) {
+                            case 'WIDGET_OVERLAP':
+                                _self.f_hui_process_wigetOverlap(data);
+                                break;
+                            case 'API_WAITING':
+                                _self.f_hui_process_apiWating(data);
+                                break;
+                        }
+                    },
+
+                    // #endregion
                 }
             });
 
@@ -75,11 +170,15 @@ var _mixin = {
 
 _app = new Vue({
     el: '#app',
+    replace: false,
     data: {
         currentView: 'home'
     },
     created: function () {
         f_log('VUE created ...');
+    },
+    events: {
+        BROADCAST_TO_VUE: 'f_receiver_messageFromComponents'
     },
     components: {
         com1: {
@@ -157,6 +256,9 @@ _app = new Vue({
 
     },
     methods: {
+        f_receiver_messageFromComponents: function (m) {
+            f_log('====> BROADCAST_TO_VUE = ', m);
+        },
         f_destroy: function () {
             this.$destroy();
         }
